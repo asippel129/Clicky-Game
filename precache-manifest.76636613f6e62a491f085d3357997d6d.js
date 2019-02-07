ba83749bbe31079b2420aaf7bf9bd3c7a9651893
Anna Sippel <asippel129@gmail.com>
2019-02-07 15:31:31 -0700

    Updates

---
+++ precache-manifest.76636613f6e62a491f085d3357997d6d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "43213ab5a6f3c14720b2",
+    "url": "/Clicky-Game/static/css/main.8c00feb5.chunk.css"
+  },
+  {
+    "revision": "43213ab5a6f3c14720b2",
+    "url": "/Clicky-Game/static/js/main.43213ab5.chunk.js"
+  },
+  {
+    "revision": "fa842ade0c2b87568f9a",
+    "url": "/Clicky-Game/static/js/1.fa842ade.chunk.js"
+  },
+  {
+    "revision": "2f9ed21177874c072eef",
+    "url": "/Clicky-Game/static/js/runtime~main.2f9ed211.js"
+  },
+  {
+    "revision": "8f739f2adf861fc416338cafa0896913",
+    "url": "/Clicky-Game/static/media/creed1.8f739f2a.jpg"
+  },
+  {
+    "revision": "f608f3f1c059952a81cbf56e36c44d5c",
+    "url": "/Clicky-Game/static/media/dwight1.f608f3f1.jpg"
+  },
+  {
+    "revision": "f574c83133642c8b6b2350dfb572e48a",
+    "url": "/Clicky-Game/static/media/dwight3.f574c831.jpg"
+  },
+  {
+    "revision": "79aa3fa6cce1c9372c59238688f60c98",
+    "url": "/Clicky-Game/static/media/kevin1.79aa3fa6.jpg"
+  },
+  {
+    "revision": "e02b8d9addde0c5dee73f62be38d9615",
+    "url": "/Clicky-Game/static/media/michael1.e02b8d9a.png"
+  },
+  {
+    "revision": "6581bdc6e76a7f37df86d93b11b4c4ff",
+    "url": "/Clicky-Game/static/media/michael2.6581bdc6.png"
+  },
+  {
+    "revision": "09236a0f6ed856d8caa6bbe9da906b99",
+    "url": "/Clicky-Game/static/media/michael3.09236a0f.png"
+  },
+  {
+    "revision": "a3760a5bf449b8ad439095768789435a",
+    "url": "/Clicky-Game/static/media/michael4.a3760a5b.png"
+  },
+  {
+    "revision": "c38c92085f81ca55dfe7a5b45dfefd87",
+    "url": "/Clicky-Game/static/media/pam1.c38c9208.jpg"
+  },
+  {
+    "revision": "ededbe85aa99dbb04aa4f91cd4438477",
+    "url": "/Clicky-Game/static/media/pam2.ededbe85.jpg"
+  },
+  {
+    "revision": "89952ca336f317587339a06dfd3b5b75",
+    "url": "/Clicky-Game/static/media/pam3.89952ca3.jpg"
+  },
+  {
+    "revision": "f3728dd93db8a26ebf6155bd559ebbbd",
+    "url": "/Clicky-Game/static/media/stanley1.f3728dd9.png"
+  },
+  {
+    "revision": "298f7921605fb18f6916e579ae1d2ec6",
+    "url": "/Clicky-Game/index.html"
+  }
+];
